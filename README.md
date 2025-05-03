92b2501699a1a0542315e4155cd0d90a94f9222d
ryanhekman <ryanhekman4@gmail.com>
2025-05-03 12:45:32 -0400

    fixed a bug in time_horizons.py (function args passed in wrong order) added some instructions to README

--- README.md
+++ README.md
@@ -6,4 +6,12 @@ Theory, optimization formulation, and algorithms are based on the work from Ulla
 This project was completed for as a final project for EC500 at Boston University (Spring 2025).
 Project contributors: Ryan Hekman, Kelsey Sweeney, and Ben Glass
 
+Scripts:
+	code.py is a standalone script that optimizes power injection of four nodes over four timesteps
+	energy_trade_function.py is a script that refactors much of code.py into a function, most min/max parameters are hard-coded, whereas schedulable injections and injection targets are arguments. Returns all optimal trades for all times.
+	time_horizons.py includes energy_trade_function, calls the solver iteratively, walking through a block of schedulable parameters. Every iteration, it recalculates and updates initial battery states for the next round.
 
+To run time_horizons.py:
+	input the desired scheduled injections, utility targets, utility priority (how much the utility cares about meeting the target for that indexed time), and initial battery states for time zero.
+	Make sure row number for scheduled injections matches number of entries for utility targets and priorities.
+	Initial battery state should always be 3 entries (system has 3 batteries)
--- time_horizons.py
+++ time_horizons.py
@@ -4,8 +4,8 @@ from scipy import linalg as linearalgebra
 from energy_trade_function import solve
 
 #how much the utility wants to receive (net) from the prosumers
-utilitytarget = np.array([-20, -20, 10, -5, 10, -5])
-utilitypriority = np.array([10, 0, 2, 0, 5, 0])
+utilitytarget = np.array([0, 10, 0, 0, 40, 0])
+utilitypriority = np.array([0, 1, 0, 0, 10, 0])
 
 prosumerschedule = np.array([[-2.8, -5, -5],
                              [0.8, 3.4, 3.4],
@@ -15,11 +15,17 @@ prosumerschedule = np.array([[-2.8, -5, -5],
                              [-3, -3, -3]])
 
 
+#make sure these are between these min/max values for each battery, the min/max are hard-coded into the solver but would be user-defined
+#0: 2/12
+#1: 10/12
+#2: 4/22
 initialbatterystate = np.array([6, 12, 11])
 
 
+all_results = np.empty((0, 64))
+
 for iteration in range(len(prosumerschedule) - 3):
-  results = solve(prosumerschedule[iteration:iteration+4], initialbatterystate, utilitypriority[iteration:iteration+4], utilitytarget[iteration:iteration+4])
+  results = solve(prosumerschedule[iteration:iteration+4], initialbatterystate, utilitytarget[iteration:iteration+4], utilitypriority[iteration:iteration+4])
 
 
 
@@ -31,28 +37,7 @@ for iteration in range(len(prosumerschedule) - 3):
       results[i] = 0
     results[i] = round(results[i], decimal_precision)
 
-  # printing the output
-  #for i in range(64):
-  #  print(timesteps[i // 16], '--', decision_variables[i % 16], ':  ', results.x[i], 'per unit')
 
-  # unsure what this is, but had to move it down below the solver
-  transform = [[0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-               [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0],
-               [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1]]
-
-  fourTransform = linearalgebra.block_diag(transform, transform, transform, transform)
-
-
-
-  # ----------------------------------------------------- Recovering P_injected_battery = Sum_P_ij - P_scheduled_inj
-
-  # This block of code sums all trades from a particular node to all other nodes at timestep t into an array structure like below.
-  # ts = 0, from node 0 | ts = 1, from node 1 | ts = 1, from node 2 | ts = 1, from node 3 |
-  # ts = 1, from node 0 | ts = 2, from node 1 | ts = 2, from node 2 | ts = 2, from node 3 |
-  # ts = 2, from node 0 | ts = 3, from node 1 | ts = 3, from node 2 | ts = 3, from node 3 |
-  # ts = 3, from node 0 | ts = 4, from node 1 | ts = 4, from node 2 | ts = 4, from node 3 |
-
-  idx = 0
 
   # excludes any injection at the slack! This only looks at the sum of the powers at bus 1, 2, and 3
   energyadded = np.zeros((12, 12))
@@ -93,3 +78,5 @@ for iteration in range(len(prosumerschedule) - 3):
 
   #resets the initial battery state for the next loop
   initialbatterystate = np.ndarray.flatten(battery_state[3:6])
+
+
